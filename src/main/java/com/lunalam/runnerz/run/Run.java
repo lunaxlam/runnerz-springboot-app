48d337ec9bdb15fe9bc64d4d119000f85b4df39b
Luna Lam <lunalam@Lunas-MacBook-Pro.local>
2024-03-24 11:24:13 -0700

    added validations dependency and custom exceptions

--- src/main/java/com/lunalam/runnerz/run/Run.java
+++ src/main/java/com/lunalam/runnerz/run/Run.java
@@ -1,13 +1,24 @@
 package com.lunalam.runnerz.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 // Record type inherently contains getters, setters, toString(), etc.
 public record Run(
         Integer id,
+        @NotEmpty
         String title,
         LocalDateTime startedOn,
         LocalDateTime completedOn,
+        @Positive
         Integer miles,
         Location location
-) {}
+) {
+    public Run {
+        if (!completedOn.isAfter((startedOn))) {
+            throw new IllegalArgumentException("Completed On must be after Started On");
+        }
+    }
+}
