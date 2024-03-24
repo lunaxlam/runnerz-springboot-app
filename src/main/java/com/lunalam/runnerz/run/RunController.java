739efac0d26aeea243a7299ad01d9a074f4f74f5
Luna Lam <lunalam@Lunas-MacBook-Pro.local>
2024-03-24 11:15:08 -0700

    added crud methods and mappings

--- src/main/java/com/lunalam/runnerz/run/RunController.java
+++ src/main/java/com/lunalam/runnerz/run/RunController.java
@@ -1,12 +1,15 @@
 package com.lunalam.runnerz.run;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 //@RestController responds to requests
 @RestController
+@RequestMapping("/api/runs")
 public class RunController {
 
     public final RunRepository runRepository;
@@ -15,10 +18,36 @@ public class RunController {
         this.runRepository = runRepository;
     }
 
-    @GetMapping("/api/runs")
+    @GetMapping("")
     List<Run> findAll() {
         return runRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    Run findById(@PathVariable Integer id) {
+        Optional<Run> run = runRepository.findById(id);
+        if (run.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found");
+        }
+        return run.get();
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    void create(@RequestBody Run run) {
+        runRepository.create(run);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void update(@PathVariable Integer id, @RequestBody Run run) {
+        runRepository.update(run, id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void delete(@PathVariable Integer id) {
+        runRepository.delete((id));
+    }
 
 }
--- src/main/java/com/lunalam/runnerz/run/RunRepository.java
+++ src/main/java/com/lunalam/runnerz/run/RunRepository.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class RunRepository {
@@ -18,6 +19,26 @@ public class RunRepository {
         return runs;
     }
 
+    Optional<Run> findById(Integer id) {
+        return runs.stream()
+                .filter(run -> run.id() == id)
+                .findFirst();
+    }
+
+    void create(Run run){
+        runs.add(run);
+    }
+
+    void update(Run run, Integer id) {
+        Optional<Run> existingRun = findById(id);
+        if(existingRun.isPresent()) {
+            runs.set(runs.indexOf(existingRun.get()), run);
+        }
+    }
+
+    void delete(Integer id) {
+        runs.removeIf(run -> run.id().equals(id));
+    }
     // After class is created, this method gets created due to @PostConstruct
     @PostConstruct
     private void init() {
