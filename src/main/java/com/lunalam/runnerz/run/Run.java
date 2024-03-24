6e26becd17b87589b07d35fcfba4985a9e651c5c
Luna Lam <lunalam@Lunas-MacBook-Pro.local>
2024-03-24 08:56:42 -0700

    Added getters and setters, etc. to Run.java

--- src/main/java/com/lunalam/runnerz/run/Run.java
+++ src/main/java/com/lunalam/runnerz/run/Run.java
@@ -1,4 +1,115 @@
 package com.lunalam.runnerz.run;
 
+import org.springframework.beans.factory.parsing.Location;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Run {
+
+    private Integer id;
+    private String title;
+    private LocalDateTime startedOn;
+    private LocalDateTime completedOn;
+    private Integer miles;
+    private Location location;
+
+    // Constructor method
+    public Run(Integer id, String title,
+               LocalDateTime startedOn,
+               LocalDateTime completedOn,
+               Integer miles,
+               Location location) throws IllegalAccessException {
+        this.id = id;
+        this.title = title;
+        this.startedOn = startedOn;
+        this.completedOn = completedOn;
+        this.miles = miles;
+        this.location = location;
+        if(!completedOn.isAfter(startedOn)) {
+            throw new IllegalAccessException("Completed On must be after Started On!");
+        }
+    }
+
+    public Duration getDuration() {
+        return Duration.between(startedOn,completedOn);
+    }
+
+    public Integer getAvgPace() {
+        return Math.toIntExact(getDuration().toMinutes() / miles);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDateTime getStartedOn() {
+        return startedOn;
+    }
+
+    public void setStartedOn(LocalDateTime startedOn) {
+        this.startedOn = startedOn;
+    }
+
+    public LocalDateTime getCompletedOn() {
+        return completedOn;
+    }
+
+    public void setCompletedOn(LocalDateTime completedOn) {
+        this.completedOn = completedOn;
+    }
+
+    public Integer getMiles() {
+        return miles;
+    }
+
+    public void setMiles(Integer miles) {
+        this.miles = miles;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Run run = (Run) o;
+        return Objects.equals(id, run.id) && Objects.equals(title, run.title) && Objects.equals(startedOn, run.startedOn) && Objects.equals(completedOn, run.completedOn) && Objects.equals(miles, run.miles) && Objects.equals(location, run.location);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, startedOn, completedOn, miles, location);
+    }
+
+    @Override
+    public String toString() {
+        return "Run{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", startedOn=" + startedOn +
+                ", completedOn=" + completedOn +
+                ", miles=" + miles +
+                ", location=" + location +
+                '}';
+    }
 }
